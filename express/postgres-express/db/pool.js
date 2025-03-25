b9c32b90504baea34cfb46da8c8c956bb65c10a6
ranhoodlum <ranhoodlum17@gmail.com>
2025-03-25 09:02:16 +0545

    feat: Implement what we did earlier, with dotenv and env variables

--- express/postgres-express/db/pool.js
+++ express/postgres-express/db/pool.js
@@ -1,19 +1,8 @@
 const { Pool } = require("pg");
 
-// Pool is a way pg connects with the database.
-//
-// client is the other way
-//
-// information inside the object is all connection information
-// read ALL properties from environment variables (later)
-module.exports = new Pool({
-  host: process.env.DB_HOST, // or whereever the database is hosted
-  // default is localhost
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DB_PORT, // default port
-});
+// create a default client pool with
+// default options
+module.exports = new Pool();
 
 // Same thing
 // // Again, this should be read from an environment variable
