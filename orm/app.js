cfc8879007a499a1432f8a1b42b8c83000ab2459
ranhoodlum <ranhoodlum17@gmail.com>
2025-04-17 15:19:14 +0545

    feat: Initialize prisma client

---
+++ orm/app.js
@@ -0,0 +1,17 @@
+// docs are confusing, because they tell you to change
+// the folder where the prismaclient is generated to
+// ../generated/prisma (default path is ./generated/prisma)
+// but they tell you to require from "@prisma/client", which
+// causes errors
+//
+// note that the paths provided above where we specify prisma
+// where to generate our prisma client (thus, ../generated/prisma is the
+// root of our project)
+// are relative to the prisma module and not
+// our project.
+const { PrismaClient } = require("./generated/prisma/");
+const prisma = new PrismaClient();
+
+prisma.users.findMany().then((userList) => {
+  console.log(userList);
+});
